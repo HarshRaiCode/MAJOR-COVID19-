90a6641db95b783d9252c9febdcd9c39b95b0101
Harsh <ramharshrai1999@gmail.com>
2022-07-28 11:48:17 +0530

    uploaded

---
+++ src/models/register.js
@@ -0,0 +1,19 @@
+const mongoose= require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    feedback: {
+        type: String,
+        required: true
+    }
+})
+
+const Register = new mongoose.model('Feedback', employeeSchema );
+module.exports = Register;
